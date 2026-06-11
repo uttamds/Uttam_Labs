56ef5a754b2ab9ecf43393ba826bbec0f8baa242
Uttam Shreyakar <39872039+uttamds@users.noreply.github.com>
2026-06-11 10:56:56 +0530

    Update travel.py

--- CIMS/Python/travel.py
+++ CIMS/Python/travel.py
@@ -1,9 +1,14 @@
 def calculate_fare():
     return 5000
 
-def book_trip():
+def total_bill():
     fare = calculate_fare()   # Consuming another function
+    gst = 500
+    return fare + gst
+
+def book_trip():
+    bill = total_bill()       # Consuming another function
     print("Trip booked.")
-    print("Fare:", fare)
+    print("Total Bill:", bill)
 
 book_trip()
